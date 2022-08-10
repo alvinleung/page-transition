55a0ab2c053a50e9e58104697a8c7bb4f6b4549b
alvin leung <alvinleung@d207-023-208-189.wireless.sfu.ca>
2022-08-10 16:50:40 -0700

    Run script when setting innerHTML

--- src/page-transition-router/router.ts
+++ src/page-transition-router/router.ts
@@ -123,7 +123,18 @@ export function createRouter(routerConfig: RouterConfig): Router {
   });
 
   function swapBody(newBodyString: string) {
-    document.body.innerHTML = newBodyString;
+    const setInnerHTMLWithScript = function (elm: HTMLElement, html: string) {
+      elm.innerHTML = html;
+      Array.from(elm.querySelectorAll("script")).forEach((oldScript) => {
+        const newScript = document.createElement("script");
+        Array.from(oldScript.attributes).forEach((attr) =>
+          newScript.setAttribute(attr.name, attr.value)
+        );
+        newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+        oldScript.parentNode?.replaceChild(newScript, oldScript);
+      });
+    };
+    setInnerHTMLWithScript(document.body, newBodyString);
   }
 
   function navigateTo(newRoute: string) {
