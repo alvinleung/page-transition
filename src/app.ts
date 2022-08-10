6dd2c073a85d889ae18fc12e062869f7d198c666
alvin leung <alvinleung@d207-023-208-189.wireless.sfu.ca>
2022-08-10 13:24:08 -0700

    Exposing router api

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,8 @@ import { createRouter, Router } from "./page-transition-router/router";
 
 // exposing the router object
 //@ts-ignore
-const router = createRouter({
+window.router = createRouter({
   onLoadRoute: () => {
     // console.log("document loaded");
   },
 });
-
-//@ts-ignore
-// const router: Router = window.router as Router;
